8692f957f0333a24625c703ec4b5424ac5e024ac
Capfa <capfa81@mail.ru>
2022-06-07 10:42:13 +0300

    exam2

--- src/ru/itmo/exam2/Command.java
+++ src/ru/itmo/exam2/Command.java
@@ -5,24 +5,7 @@ import java.io.IOException;
 
 public interface Command {
     void execute();
-   /* static Command getInstance(int actionNumber,Game game){
-        if (actionNumber==1) {
 
-            return (RunCommand(game));
-        } else if (actionNumber==2){
-            LoadGame loadGame=new LoadGame();
-            return (loadGame.loadGame());
-        } else {
-            return () -> {
-                try {
-                    server.getConnection()
-                            .sendMessage(SimpleMessage.getMessage("server", "команда не может быть обработана"));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            };
-        }
-    }*/
 
 
 }
